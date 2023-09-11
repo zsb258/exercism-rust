a6d1c11b90ffe24c033e3dd7b6f4997c9cecfc81
zsb258 <zhuang.shengbin@u.nus.edu>
2023-09-11 16:58:50 +0800

    done: luhn v2

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,43 +1,24 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let code = code.replace(" ", "");
-    if code.len() <= 1 {
-        false
-    } else {
-        is_valid_impl(code)
-    }
+    code.chars()
+        .filter(|c| !c.is_whitespace())
+        .try_rfold((0, 0), |(i, sum), c| {
+            c.to_digit(10)
+                .map(|n| {
+                    if is_odd_pos(i) {
+                        n
+                    } else {
+                        match n {
+                            9 => 9,
+                            x => (x * 2) % 9,
+                        }
+                    }
+                })
+                .map(|n| (i + 1, sum + n))
+        })
+        .map_or(false, |(count, sum)| count > 1 && sum % 10 == 0)
 }
 
 fn is_odd_pos(i: usize) -> bool {
     i % 2 == 0
-}
-
-fn is_valid_impl(code: String) -> bool {
-    let num_options = code.chars().map(|c| c.to_digit(10));
-
-    if num_options.clone().any(|x| x.is_none()) {
-        return false;
-    }
-
-    let checksum = num_options
-        .map(|x| x.unwrap())
-        .rev()
-        .enumerate()
-        .map(|(i, n)| {
-            if is_odd_pos(i) {
-                n
-            } else {
-                match n {
-                    9 => 9,
-                    x => (x * 2) % 9,
-                }
-            }
-        })
-        .fold(0, |acc, n| acc + n);
-
-    if checksum % 10 == 0 {
-        true
-    } else {
-        false
-    }
 }
